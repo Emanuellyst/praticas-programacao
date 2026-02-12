f9f307ef7814751f9eefa09100f9945c834c7761
Emanuelly Santos <emanuellycsf@gmail.com>
2026-02-11 23:59:24 -0300

    Change temperature output precision to one decimal

--- linguagens/python/exercicios/projetos_simples/conversor_temperatura.py
+++ linguagens/python/exercicios/projetos_simples/conversor_temperatura.py
@@ -10,12 +10,12 @@ if opcao == "1":
     temp_c = float(input("Qual é a temperatura em (°C) atual? ")) # Python aceita apenas ponto (.) como separador decimal
 
     f = temp_c*1.8+32
-    print(f"Está fazendo aproximadamente {f:.2f} °F")
+    print(f"Está fazendo aproximadamente {f:.1f} °F")
 
 elif opcao == "2":
     temp_f = float(input("Qual é a temperatura em (°F) atual? "))
     c = (temp_f-32)/1.8
-    print(f"Está fazendo aproximadamente {c:.2f} °C")
+    print(f"Está fazendo aproximadamente {c:.1f} °C")
 
 else:
     print("Digite uma opção válida (1 ou 2).")
